97f4cff9a8ff1dc38ea35f7a4a82748d786ebce1
Richie Bendall <richiebendall@gmail.com>
2025-07-20 01:29:18 +1200

    Add type test

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,3 +4,4 @@ import emailRegex from './index.js';
 expectType<RegExp>(emailRegex());
 expectType<RegExp>(emailRegex({}));
 expectType<RegExp>(emailRegex({exact: true}));
+expectType<RegExp>(emailRegex({exact: true, allowAmpersandEntity: true, allowInternalDomain: true}));
